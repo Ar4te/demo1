b8744c98bab2b6018323dbf24b54e8bbef7d6ad5
Ar4te <1101458943@qq.com>
2023-03-04 01:06:16 +0800

    add login API

--- controller/UserController.go
+++ controller/UserController.go
@@ -7,6 +7,7 @@ import (
 	"ginDemo/model"
 	"ginDemo/common"
 	"ginDemo/util"
+	"golang.org/x/crypto/bcrypt"
 )
 
 
@@ -45,10 +46,18 @@ func Register (c *gin.Context){
 		return
 	}
 	//创建用户
+	hasedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"code":500,
+			"msg":"加密失败！",
+		})
+		return
+	}
 	newUser := model.User {
 		Name: name,
 		Telephone: telephone,
-		Password: password,
+		Password: string(hasedPassword),
 	}
 
 	DB.Create(&newUser)
@@ -57,6 +66,59 @@ func Register (c *gin.Context){
 		"msg":"注册成功",
 	})
 	log.Println(name, telephone, password)
+}
+
+func Login (c *gin.Context){
+	DB := common.GetDB()
+	//获取参数
+	// name := c.PostForm("name")
+	telephone := c.PostForm("telephone")
+	password := c.PostForm("password")
+	//数据验证
+	if len(telephone) != 11 {
+		c.JSON(422, gin.H{
+			"code":422,
+			"msg":"手机号必须为11位",
+		})
+		return
+	}
+
+	if len(password) < 6 {
+		c.JSON(422, gin.H{
+			"code":422,
+			"msg":"密码不能少于6位",
+		})
+		return
+	}
+
+	//验证手机号是否存在
+	var user model.User
+	DB.Where("telephone = ?", telephone).First(&user)
+	if user.ID == 0 {
+		c.JSON(422, gin.H{
+			"code":422,
+			"msg":"用户不存在",
+		})
+		return
+	}
+	//判断密码是否正确
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		c.JSON(400, gin.H{
+			"code":400,
+			"msg":"密码错误",
+		})
+		return
+	}
+
+	//发放token
+	token := "111"
+
+	//返回结果
+	c.JSON(200, gin.H{
+		"code":200,
+		"data": gin.H{"token":token},
+		"msg":"登陆成功",
+	})
 }
 
 
