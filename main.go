1eca68d53b1231455aa27b6eebbbc2cbeea4bbee
Ar4te <1101458943@qq.com>
2023-03-29 23:36:56 +0800

    更新结构

--- main.go
+++ main.go
@@ -2,34 +2,24 @@ package main
 
 import (
 	"ginDemo/common"
+	"ginDemo/config"
+	"ginDemo/router"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/spf13/viper"
-	"os"
 )
 
 func main() {
-	InitConfig()
+	config.InitConfig()
 
 	db := common.InitDB()
 	defer db.Close()
 
 	r := gin.Default()
-	r = CollectRoute(r)
+	r = router.CollectRoute(r)
 	port := viper.GetString("server.port")
 	if port != "" {
 		panic(r.Run(":" + port))
 	}
 	panic(r.Run(":5200"))
-}
-
-func InitConfig() {
-	workDir, _ := os.Getwd()
-	viper.SetConfigName("application")
-	viper.SetConfigType("yml")
-	viper.AddConfigPath(workDir + "/config")
-	err := viper.ReadInConfig()
-	if err != nil {
-		panic("read config err:" + err.Error())
-	}
 }
---
+++ router/routers.go
@@ -0,0 +1,65 @@
+package router
+
+import (
+	"ginDemo/controller"
+	"ginDemo/middleware"
+	"github.com/didip/tollbooth"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/testdata/protoexample"
+)
+
+func CollectRoute(r *gin.Engine) *gin.Engine {
+	lmt := tollbooth.NewLimiter(1, nil)
+	lmt.SetMessage("服务繁忙，请稍后再试。。。")
+
+	//user
+	{
+		r.POST("/api/auth/register", controller.Register)
+		r.POST("/api/auth/login", controller.Login)
+		r.GET("/api/auth/info", middleware.AuthMiddleware(), controller.Info)
+		r.POST("/api/auth/rename", middleware.AuthMiddleware(), controller.ReName)
+		r.POST("/api/auth/rePwd", middleware.AuthMiddleware(), controller.RePassword)
+		r.GET("/api/auth/testId", controller.TestId)
+	}
+	// article
+	{
+		r.POST("/api/article/upload", controller.UploadFile)
+		r.GET("/api/article/download", controller.DownloadFile)
+		r.GET("/api/article/getAll", middleware.LimitHandler(lmt), middleware.AuthMiddleware(), controller.GetAllArticle)
+		r.POST("/api/article/updateFile", middleware.AuthMiddleware(), controller.UpdateFile)
+		r.DELETE("/api/article/delFile", middleware.AuthMiddleware(), controller.DeleteFile)
+	}
+	// test
+	{
+		r.GET("/api/test/somJSON", func(c *gin.Context) {
+			c.JSON(200, gin.H{"message": "someJSON", "status": 200})
+		})
+		r.GET("/api/test/someStruct", func(c *gin.Context) {
+			var msg struct {
+				Name    string
+				Message string
+				Number  int
+			}
+			msg.Name = "root"
+			msg.Message = "message"
+			msg.Number = 123
+			c.JSON(200, msg)
+		})
+		r.GET("/api/test/someXML", func(c *gin.Context) {
+			c.XML(200, gin.H{"message": "abc"})
+		})
+		r.GET("/api/test/someYAML", func(c *gin.Context) {
+			c.YAML(200, gin.H{"name": "zhangsan"})
+		})
+		r.GET("/api/test/someProtoBuf", func(c *gin.Context) {
+			reps := []int64{int64(1), int64(2)}
+			label := "label"
+			data := &protoexample.Test{
+				Label: &label,
+				Reps:  reps,
+			}
+			c.ProtoBuf(200, data)
+		})
+	}
+	return r
+}
